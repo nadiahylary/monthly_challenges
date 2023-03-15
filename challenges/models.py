c07f1e67aeb3db52fdb3888403ff55d437a6ba8a
Nadia HYLARY <nhylary12@gmail.com>
2023-03-15 22:13:43 +0100

    added model and admin conf

--- challenges/models.py
+++ challenges/models.py
@@ -2,5 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+
 class Challenge(models.Model):
-    pass
+    month = models.CharField(max_length=20)
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=500)
